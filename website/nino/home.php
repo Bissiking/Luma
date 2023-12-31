ade097b292556786dcd9fa49fd241a9ec2a5ca6e
Bissiking <34923959+Bissiking@users.noreply.github.com>
2023-12-31 13:04:54 +0100

    4.0.0
    - Récupération des vidéos depuis l'API
    - Patch du menu

--- website/nino/home.php
+++ website/nino/home.php
@@ -5,7 +5,41 @@
 
 <h1>Mes Vidéos</h1>
 
-<div class="video">
+<?php
+// URL de l'API YouTube Data v3 pour récupérer les vidéos de la chaîne
+$apiUrl = "https://nino.mhemery.fr/api/videos";
+
+// Effectuez la requête vers l'API YouTube
+$response = file_get_contents($apiUrl);
+$data = json_decode($response);
+
+// Vérifiez si la requête a réussi
+if ($data) {
+    // Affichez les vidéos
+    foreach ($data as $video) {
+        echo '<pre>';
+        print_r($video);
+        echo'</pre>';
+
+        $videoId = $video->id;
+        $videoTitle = $video->title;
+        $videoDescription = $video->description;
+        $videoThumbnail = "https://99designs-blog.imgix.net/blog/wp-content/uploads/2016/03/web-images.jpg?auto=format&q=60&w=1600&h=824&fit=crop&crop=faces";
+        // URL Image
+
+        echo "<div class='video'>";
+        echo "<img src='{$videoThumbnail}' alt='{$videoTitle}'>";
+        echo "<div class='video-info'>";
+        echo "<div class='video-title'>{$videoTitle}</div>";
+        echo "<div class='video-description'>{$videoDescription}</div>";
+        echo "<button class='subscribe-button'>S'abonner</button>";
+        echo "</div></div>";
+    }
+} else {
+    echo "Erreur lors de la récupération des vidéos. Soit il n'y a pas de vidéo, soit l'API est en maintenance. Retente plus tard";
+} ?>
+
+<!-- <div class="video">
     <img src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2016/03/web-images.jpg?auto=format&q=60&w=1600&h=824&fit=crop&crop=faces" alt="Vidéo 1 Thumbnail">
     <div class="video-info">
         <div class="video-title">Titre de la Vidéo 1</div>
@@ -21,6 +55,6 @@
         <div class="video-description">Description de la vidéo 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
         <button class="subscribe-button">Regarder la vidéo</button>
     </div>
-</div>
+</div> -->
 
 <!-- SCRIPTS SRV -->
