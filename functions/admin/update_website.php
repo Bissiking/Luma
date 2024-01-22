6cf9f0921f1c8b9e6a8189715770c311a540d584
Bissiking <34923959+Bissiking@users.noreply.github.com>
2024-01-22 04:24:19 +0100

    pull site

--- functions/admin/update_website.php
+++ functions/admin/update_website.php
@@ -11,11 +11,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // TEST FUNCTION EDIT CONST
     $const = ConstEdit('WEB_MAINTENANCE', 'true');
-    var_dump($const);
 
-    // Exemple : pull depuis le dépôt Git
-    $output = shell_exec('git pull origin pre-prod'); // Assurez-vous de changer "master" si vous utilisez une autre branche
+    try {
+        $gitPath = 'C:/Program Files/Git/bin/git.exe';  // Remplacez par le chemin réel de l'exécutable Git
+
+        // Ajoutez le chemin de Git au PATH
+        putenv("PATH=" . getenv("PATH") . ";" . dirname($gitPath));
+        // Changer le répertoire de travail
+        chdir("../../");
+
+        // Exécuter la commande Git
+        $output = shell_exec('git stash && git pull 2>&1');
+
+        if ($output === null) {
+            throw new Exception('La commande git stash n\'a pas retourné de sortie.');
+        }
+
+        // Afficher la sortie
+        echo 'succes';
+    } catch (Exception $e) {
+        // Gérer les exceptions
+        echo 'Erreur : ' . $e->getMessage();
+    }
+    $const = ConstEdit('WEB_MAINTENANCE', 'false');
 
     // Afficher le résultat
-    echo nl2br($output);
+    echo nl2br('OUT: ' . $output);
 }
+
+$const = ConstEdit('WEB_MAINTENANCE', 'false');
