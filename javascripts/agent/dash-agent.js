09e68bd0e72273145eb883f30dc51f99c06e8165
Bissiking <34923959+Bissiking@users.noreply.github.com>
2024-03-06 20:58:57 +0100

    5.0

--- javascripts/agent/dash-agent.js
+++ javascripts/agent/dash-agent.js
@@ -1,17 +1,57 @@
 var uuid = $('#agent-uuid').text();
 var url = "/data/" + uuid + "/";
 
+const currentTime = new Date();
+
+
 function ModelProcessor() {
     $.ajax({
         url: url + "processor-data.json",
         dataType: 'json',
         success: function (response) {
             let data = response.result.status.cpu;
-            $('#data-agent-ModelProcessor').text(data.model)
-            $('#data-agent-architecture').text(data.architecture)
-            $('#data-agent-Cores').text(data.cores)
-            $('#data-agent-Used').text(data.usage)
-            $('#data-agent-processorUpdate').text(response.result.status.date)
+
+            // Calcule du temps d'actualisation de la sonde
+
+            var sondeTime = new Date(response.result.status.date);
+            var elapsedTimeInSeconds = (currentTime - sondeTime) / 1000;
+
+            if (elapsedTimeInSeconds > 180) {
+                // Avertissement alerte sonde
+                $('#config-sys').removeClass();
+                $('#config-sys').addClass('sonde-error');
+
+                // Calcule en Heure lisible
+                const jours = Math.floor(elapsedTimeInSeconds / (3600 * 24));
+                const heures = Math.floor((elapsedTimeInSeconds % (3600 * 24)) / 3600);
+                const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+                const secondes = Math.floor(elapsedTimeInSeconds % 60);
+
+                // Message d'alerte de non contact de la sonde
+                $('#config-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else if (elapsedTimeInSeconds > 60) {
+                // Avertissement alerte sonde
+                $('#config-sys').removeClass();
+                $('#config-sys').addClass('sonde-warning');
+
+                // Calcule en Heure lisible
+                const jours = Math.floor(elapsedTimeInSeconds / (3600 * 24));
+                const heures = Math.floor((elapsedTimeInSeconds % (3600 * 24)) / 3600);
+                const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+                const secondes = Math.floor(elapsedTimeInSeconds % 60);
+
+                // Message d'alerte de non contact de la sonde
+                $('#config-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else {
+                $('#config-sys').removeClass();
+                $('#config-sys-alerte').text(`RAS`);
+            }
+
+            $('#data-agent-ModelProcessor').text(data.model);
+            $('#data-agent-architecture').text(data.architecture);
+            $('#data-agent-Cores').text(data.cores);
+            $('#data-agent-Used').text(data.usage);
+            $('#data-agent-processorUpdate').text(response.result.status.date);
         },
         error: function (error) {
             console.error('Erreur lors du chargement du fichier JSON :', error);
@@ -25,6 +65,42 @@ function Memory() {
         dataType: 'json',
         success: function (response) {
             let data = response.result.status.memory;
+
+            var sondeTime = new Date(response.result.status.date);
+            var elapsedTimeInSeconds = (currentTime - sondeTime) / 1000;
+
+            if (elapsedTimeInSeconds > 180) {
+                // Avertissement alerte sonde
+                $('#memory-sys').removeClass();
+                $('#memory-sys').addClass('sonde-error');
+
+                // Calcule en Heure lisible
+                const jours = Math.floor(elapsedTimeInSeconds / (3600 * 24));
+                const heures = Math.floor((elapsedTimeInSeconds % (3600 * 24)) / 3600);
+                const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+                const secondes = Math.floor(elapsedTimeInSeconds % 60);
+
+                // Message d'alerte de non contact de la sonde
+                $('#memory-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else if (elapsedTimeInSeconds > 60) {
+                // Avertissement alerte sonde
+                $('#memory-sys').removeClass();
+                $('#memory-sys').addClass('sonde-warning');
+
+                // Calcule en Heure lisible
+                const jours = Math.floor(elapsedTimeInSeconds / (3600 * 24));
+                const heures = Math.floor((elapsedTimeInSeconds % (3600 * 24)) / 3600);
+                const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+                const secondes = Math.floor(elapsedTimeInSeconds % 60);
+
+                // Message d'alerte de non contact de la sonde
+                $('#memory-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else {
+                $('#memory-sys').removeClass();
+                $('#memory-sys-alerte').text(`RAS`);
+            }
+
+
             $('#data-agent-Memfree').text(data.free)
             $('#data-agent-MemTotal').text(data.total)
             $('#data-agent-memoryUpdate').text(response.result.status.date)
@@ -41,6 +117,33 @@ function ServicesPlex() {
         dataType: 'json',
         success: function (response) {
             let data = response.result;
+
+            var sondeTime = new Date(response.result.date);
+            var elapsedTimeInSeconds = (currentTime - sondeTime) / 1000;
+            // Calcule en Heure lisible
+            const jours = Math.floor(elapsedTimeInSeconds / (3600 * 24));
+            const heures = Math.floor((elapsedTimeInSeconds % (3600 * 24)) / 3600);
+            const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60);
+            const secondes = Math.floor(elapsedTimeInSeconds % 60);
+
+            if (elapsedTimeInSeconds > 180) {
+                // Avertissement alerte sonde
+                $('#plex-sys').removeClass();
+                $('#plex-sys').addClass('sonde-error');
+
+                // Message d'alerte de non contact de la sonde
+                $('#plex-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else if (elapsedTimeInSeconds > 60) {
+                // Avertissement alerte sonde
+                $('#plex-sys').removeClass();
+                $('#plex-sys').addClass('sonde-warning');
+
+                // Message d'alerte de non contact de la sonde
+                $('#plex-sys-alerte').text(`La sonde ne réponds plus depuis: ${jours} Jour(s), ${heures} Heure(s), ${minutes} minute(s) et ${secondes} seconde(s)`);
+            } else {
+                $('#plex-sys').removeClass();
+                $('#plex-sys-alerte').text(`RAS`);
+            }
 
             if (data.status == 1) {
                 statsTxt = 'En fonctionnement';
@@ -73,13 +176,80 @@ function ServicesPlex() {
 //     });
 // }
 
+function LoadDisk() {
+    let NbDisk = 0;
+    $.ajax({
+        url: url + "disk-data.json",
+        dataType: 'json',
+        success: function (response) {
+            data = response.result.status.disk;
+            NbDisk = data.length; // Attribution du nombre de disque
+
+            $('#nb-disque').text('Disques: ' + data.length);
+            for (let i = 0; i < data.length; i++) {
+                $("#block-disk").append(
+                    '<div id="disk-' + i + '" class="box gauge--3 diskBox">' +
+                    '<div class="mask">' +
+                    '<div class="semi-circle">' +
+                    '<span id="disk-' + i + '-text" class="pourcent">...</span>' +
+                    '</div>' +
+                    '<div id="disk-' + i + '-mask" class="semi-circle--mask"></div>' +
+                    '</div>' +
+                    '<p>' + data[i].mount + '</p>' +
+                    '</div>');
+            }
+        },
+        error: function (error) {
+            console.error('Erreur lors du chargement du fichier JSON :', error);
+        }
+    });
+}
+
+function ReqDisk(i) {
+    $.ajax({
+        url: url + "disk-data.json",
+        dataType: 'json',
+        success: function (response) {
+            let data = response.result.status.disk
+            // Affichage des gauges
+            let val = (Math.round(data[i].use) * 180) / 100;
+            $('#disk-' + i + '-mask').attr({
+                style: '-webkit-transform: rotate(' + val + 'deg);' +
+                    '-moz-transform: rotate(' + val + 'deg);' +
+                    'transform: rotate(' + val + 'deg);'
+            });
+            if (data == "stop") {
+                $('#disk-' + i + '-text').text("Indisponible");
+            } else {
+                $('#disk-' + i + '-text').text(Math.round(data[i].use) + "%");
+            }
+        },
+        error: function (error) {
+            console.error('Erreur lors du chargement du fichier JSON :', error);
+        }
+    });
+}
+
+function CheckDisk() {
+    let NbDiskLength = $('.diskBox').length;
+    for (let i = 0; i < NbDiskLength; i++) {
+        ReqDisk(i);
+    }
+}
+
 // FISRT START
 ModelProcessor();
 ServicesPlex();
 Memory();
+LoadDisk();
 
-setTimeout(() => {
+setInterval(() => {
+    CheckDisk();
+}, 3000);
+
+setInterval(() => {
     ModelProcessor();
     ServicesPlex();
     Memory();
-}, 20000);
+    CheckDisk();
+}, 10000);
--- website/agent/dash_agent.php
+++ website/agent/dash_agent.php
@@ -27,80 +27,102 @@ if (!isset($uuid) || $uuid == null || $uuid == "uuid") {
     document.title = "Agent - <?= $agent['agent_name'] ?>";
 </script>
 
-<section>
-    <h2>Informations Agent</h2>
-    <div class="agent-data">
-        <p class="label">Nom de l'agent:</p>
-        <p class="data-agent"><?= $agent['agent_name'] ?></p>
-    </div>
-    <div class="agent-data">
-        <p class="label">UUID:</p>
-        <p id="agent-uuid" class="data-agent"><?= $agent['uuid_agent'] ?></p>
-    </div>
-</section>
+<div class="content-dashboard-agent">
+    <section>
+        <h2>Informations Agent</h2>
+        <div class="agent-data">
+            <p class="label">Nom de l'agent:</p>
+            <p class="data-agent"><?= $agent['agent_name'] ?></p>
+        </div>
+        <div class="agent-data">
+            <p class="label">UUID:</p>
+            <p id="agent-uuid" class="data-agent"><?= $agent['uuid_agent'] ?></p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Etat de l'agent:</p>
+            <p id="agent-statut" class="data-agent">Non disponible</p>
+        </div>
+    </section>
 
-<section>
-    <h2>Configuration Système</h2>
-    <div class="agent-data">
-        <p class="label">Model Processor:</p>
-        <p id="data-agent-ModelProcessor" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Architecture:</p>
-        <p id="data-agent-architecture" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Coeurs:</p>
-        <p id="data-agent-Cores" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Utiliation:</p>
-        <p id="data-agent-Used" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Dernière mise à jour:</p>
-        <p id="data-agent-processorUpdate" class="data-agent">Chargement des données</p>
-    </div>
-</section>
+    <section id="config-sys" class="sonde-error">
+        <h2>Configuration Système</h2>
+        <div class="agent-data">
+            <p class="label">Model Processor:</p>
+            <p id="data-agent-ModelProcessor" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Architecture:</p>
+            <p id="data-agent-architecture" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Coeurs:</p>
+            <p id="data-agent-Cores" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Utiliation:</p>
+            <p id="data-agent-Used" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Dernière mise à jour:</p>
+            <p id="data-agent-processorUpdate" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Alerte:</p>
+            <p id="config-sys-alerte" class="data-agent alert">RAS</p>
+        </div>
+    </section>
 
-<section>
-    <h2>Mémoire système</h2>
-    <div class="agent-data">
-        <p class="label">Mémoire restante:</p>
-        <p id="data-agent-Memfree" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Mémoire total:</p>
-        <p id="data-agent-MemTotal" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Dernière mise à jour:</p>
-        <p id="data-agent-memoryUpdate" class="data-agent">Chargement des données</p>
-    </div>
-</section>
+    <section id="memory-sys">
+        <h2>Mémoire système</h2>
+        <div class="agent-data">
+            <p class="label">Mémoire restante:</p>
+            <p id="data-agent-Memfree" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Mémoire total:</p>
+            <p id="data-agent-MemTotal" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Dernière mise à jour:</p>
+            <p id="data-agent-memoryUpdate" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Alerte:</p>
+            <p id="memory-sys-alerte" class="data-agent alert">RAS</p>
+        </div>
+    </section>
 
-<section>
-    <h2>Disque système</h2>
-    <div class="agent-data">
-        <p class="label">Disques :</p>
-        <p id="data-agent-Memfree" class="data-agent">Non disponible</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Dernière mise à jour:</p>
-        <p id="data-agent-memoryUpdate" class="data-agent">Non disponible</p>
-    </div>
-</section>
+    <section id="disk-sys">
+        <h2>Disque système</h2>
+        <div class="agent-data">
+            <p class="label">Disques :</p>
+            <p id="data-agent-Memfree" class="data-agent">Non disponible</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Dernière mise à jour:</p>
+            <p id="data-agent-memoryUpdate" class="data-agent">Non disponible</p>
+        </div>
+    </section>
 
-<section>
-    <h2>Services</h2>
-    <div class="agent-data">
-        <p class="label">Plex :</p>
-        <p id="data-agent-plex" class="data-agent">Chargement des données</p>
-    </div>
-    <div class="agent-data">
-        <p class="label">Dernière mise à jour:</p>
-        <p id="data-agent-servicesUpdate" class="data-agent">Chargement des données</p>
-    </div>
+    <section id="plex-sys">
+        <h2>Services</h2>
+        <div class="agent-data">
+            <p class="label">Plex :</p>
+            <p id="data-agent-plex" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Dernière mise à jour:</p>
+            <p id="data-agent-servicesUpdate" class="data-agent">Chargement des données</p>
+        </div>
+        <div class="agent-data">
+            <p class="label">Alerte:</p>
+            <p id="plex-sys-alerte" class="data-agent alert">RAS</p>
+        </div>
+    </section>
+</div>
+
+<section id="block-disk" class="content-dashboard-agent-disk">
+    <h4 id="nb-disque" style="color: black">Chargement des disques</h4>
 </section>
 
 <script src="<?= SITE_HTTP . SITE_URL ?>/javascripts/agent/dash-agent.js?0"></script>
