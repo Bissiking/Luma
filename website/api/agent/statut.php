f25d6f9c11035aaf6e272860b0f5520b61bbf25a
Bissiking <34923959+Bissiking@users.noreply.github.com>
2024-03-07 10:49:16 +0100

    1.0.0 - EXP - TEST02

--- website/api/agent/statut.php
+++ website/api/agent/statut.php
@@ -8,50 +8,86 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Convertir le JSON en tableau associatif
     $data = json_decode($json, true);
-
     if ($data !== null) {
-        $uuid = $data['result']['uuid'];
-        $processName = $data['result']['processName'];
-        var_dump($data['result']['uuid']);
-        $tableName = 'luma_agent'; // Choix de la table
-        $criteria = [
-            'uuid_agent' => $uuid,
-        ];
-        $rowExists = checkRowExistence($tableName, $criteria, $pdo);
-        if ($rowExists === true) {
-            // correspondance
-            // Chemin du fichier JSON
-            $filePath = $processName.'-data.json';
-            $agentDocs = "data/$uuid";
-            
-            // Convertir les données en format JSON
-            $jsonData = json_encode($data, JSON_PRETTY_PRINT);
 
-            // Vérification de la présence du dossier
-            if (!is_dir($agentDocs)) {
-                if (!mkdir($agentDocs, 0777)) {
-                    header('Content-Type: application/json');
-                    echo json_encode(['ERROR' => 'ECHEC DOCS CREATE']);
-                    exit;
-                }
-            }
+        if ($data['config'] == true) {
+            // Vérification de la présence de l'agent
+            $tableName = "luma_agent";
+            $criteria = [
+                'uuid_agent' => $data['uuid']
+            ];
+            $rowExists = checkRowExistence($tableName, $criteria, $pdo);
+            if ($rowExists === true) {
 
-            // Écrire les données JSON dans un fichier
-            if (file_put_contents($agentDocs.'/'.$filePath, $jsonData)) {
+                // Récupération des datas agents
+                $sql = 'SELECT * FROM luma_agent WHERE uuid_agent = :uuid_agent';
+                $req = $pdo->prepare($sql);
+                $req->bindParam(':uuid_agent', $data['uuid']);
+                $req->execute();
+                $agent = $req->fetch(PDO::FETCH_ASSOC);
+
+                // Envoie de la config à l'agent
                 header('Content-Type: application/json');
-                echo json_encode(['success' => true]);
-            } else {
+                echo json_encode([
+                    'success' => true,
+                    'data' => $agent
+                ]);
+
+            } elseif ($rowExists === false) {
+                // Non identifié
                 header('Content-Type: application/json');
                 echo json_encode(['success' => false]);
+            } else {
+                header('Content-Type: application/json');
+                echo json_encode(['ERROR' => $rowExists]);
             }
-        } elseif ($rowExists === false) {
-            // Aucune correspondance
-            header('Content-Type: application/json');
-            echo json_encode(['error' => 'Agent No-Exist']);
         } else {
-            header('Content-Type: application/json');
-            echo json_encode(['error' => 'ECHEC-PDO']);
+            $data1 = $data;
         }
+
+
+        // $uuid = $data['result']['uuid'];
+        // $processName = $data['result']['processName'];
+        // var_dump($data['result']['uuid']);
+        // $tableName = 'luma_agent';
+        // $criteria = [
+        //     'uuid_agent' => $uuid,
+        // ];
+        // $rowExists = checkRowExistence($tableName, $criteria, $pdo);
+        // if ($rowExists === true) {
+        //     // correspondance
+        //     // Chemin du fichier JSON
+        //     $filePath = $processName.'-data.json';
+        //     $agentDocs = "data/$uuid";
+
+        //     // Convertir les données en format JSON
+        //     $jsonData = json_encode($data, JSON_PRETTY_PRINT);
+
+        //     // Vérification de la présence du dossier
+        //     if (!is_dir($agentDocs)) {
+        //         if (!mkdir($agentDocs, 0777)) {
+        //             header('Content-Type: application/json');
+        //             echo json_encode(['ERROR' => 'ECHEC DOCS CREATE']);
+        //             exit;
+        //         }
+        //     }
+
+        //     // Écrire les données JSON dans un fichier
+        //     if (file_put_contents($agentDocs.'/'.$filePath, $jsonData)) {
+        //         header('Content-Type: application/json');
+        //         echo json_encode(['success' => true]);
+        //     } else {
+        //         header('Content-Type: application/json');
+        //         echo json_encode(['success' => false]);
+        //     }
+        // } elseif ($rowExists === false) {
+        //     // Aucune correspondance
+        //     header('Content-Type: application/json');
+        //     echo json_encode(['error' => 'Agent No-Exist']);
+        // } else {
+        //     header('Content-Type: application/json');
+        //     echo json_encode(['error' => 'ECHEC-PDO']);
+        // }
     } else {
         // En cas d'erreur de décodage JSON
         header('Content-Type: application/json');
