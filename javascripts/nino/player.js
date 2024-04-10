f1cf33b213ae5b5e3b878d5942c9483cbae9e49d
Bissiking <34923959+Bissiking@users.noreply.github.com>
2024-04-10 05:16:04 +0200

    V5.1.0
    - Nino Player UPDATE

--- javascripts/nino/player.js
+++ javascripts/nino/player.js
@@ -13,21 +13,22 @@ var ProgressBarJquery = $('#progressBar');
 var BlockCustomControls = $('#customControls');
 var VCP = $('.video-container-player');
 var MS = $('#movies-screen');
-var PBP = $('#playPauseBtn'); 
+var PBP = $('#playPauseBtn');
+var volumeIcon = $('#volume-mute');
 // AUTRES
 var idleTimeout;
 
 // Hidden si pas de mouvement de souris
 function resetIdleTimeout() {
     clearTimeout(idleTimeout); // Effacer le délai d'inactivité existant
-    idleTimeout = setTimeout(function() {
+    idleTimeout = setTimeout(function () {
         // Aucun mouvement détecté, donc réduire l'opacité de la vidéo
         BlockCustomControls.css('opacity', 0);
     }, 5000); // Temps d'inactivité en millisecondes (ici 5 secondes)
 }
 
 // Écouter les événements de la souris et du clavier pour réinitialiser le délai d'inactivité
-VCP.on('mousemove keydown', function() {
+VCP.on('mousemove keydown', function () {
     BlockCustomControls.css('opacity', 1); // Rétablir l'opacité de la vidéo
     resetIdleTimeout(); // Réinitialiser le délai d'inactivité
 });
@@ -102,17 +103,101 @@ setInterval(() => {
 }, 500);
 
 // Volume
-// volume.addEventListener("click", function () {
-//     showPopup('info', 'Encore de la patience', 'Fonctionalité non disponible')
-// });
+$('.slider').on('input', function() {
+    let volume = $(this).val();
+    updateVolume(volume);
+});
+
+$('#volume-mute').click(function() {
+    Player.muted = !Player.muted;
+
+    // Mettez à jour le texte du bouton en fonction de l'état de la sourdine
+    if (!Player.muted) {
+        updateVolume(sessionStorage.getItem('PlayerVolume'));
+    } else {
+        volumeIcon.removeClass('fa-volume-low fa-volume-high fa-volume-off fa-volume-xmark');
+        volumeIcon.addClass('fa-volume-xmark');
+    }
+});
+
+function EditIcoVol(volume) {
+    volumeIcon.removeClass('fa-volume-low fa-volume-high fa-volume-off fa-volume-xmark');
+
+    if (Player.muted) {
+        volume = 0;
+    }
+
+    if (volume === 0) {
+        volumeIcon.addClass('fa-volume-xmark');
+    }else if (volume < 33) {
+        volumeIcon.addClass('fa-volume-off');
+    }else if (volume < 77) {
+        volumeIcon.addClass('fa-volume-low');
+    }else{
+        volumeIcon.addClass('fa-volume-high');
+    }
+}
+
+function updateVolume(VolUP) {
+    const volNew = VolUP/100;
+    const volume = Math.min(Math.max(volNew, 0), 1);
+    myVideo.volume = volume;
+    // Stockez la valeur du volume dans sessionStorage
+    sessionStorage.setItem('PlayerVolume', VolUP);
+    // Modification du logo
+    EditIcoVol(VolUP);
+}
+
+// Au chargement du Player
+if(sessionStorage.getItem('PlayerVolume') !== undefined || sessionStorage.getItem('PlayerVolume') !== "" || sessionStorage.getItem('PlayerVolume') !== null){
+    updateVolume(sessionStorage.getItem('PlayerVolume'));
+    $('.slider').attr('value', sessionStorage.getItem('PlayerVolume'))
+}else{
+    updateVolume(50);
+}
 
 // BETA TEST
-videoContainer.addEventListener("fullscreenchange", function() {
+videoContainer.addEventListener("fullscreenchange", function () {
     if (document.fullscreenElement) {
         $('video').css('height', "100vh");
         $('video').removeClass('VideoHeightMax');
+        BlockCustomControls.data('fullscreen', true)
     } else {
         $('video').css('height', "auto");
         $('video').addClass('VideoHeightMax');
+        BlockCustomControls.data('fullscreen', false)
     }
 });
+
+// Fin de vidéo
+Player.addEventListener("loadedmetadata", function() {
+    const duration = Player.duration;
+    const currentTime = new Date();
+    const endTime = new Date(currentTime.getTime() + duration * 1000);
+
+    $('#TimeEndVideo').text(formatTime(endTime));
+});
+
+setInterval(() => {
+    const currentTime = Player.currentTime;
+    const duration = Player.duration;
+
+    const remainingTime = duration - currentTime;
+    const endTime = new Date(Date.now() + remainingTime * 1000);
+
+    $('#TimeEndVideo').text(formatTime(endTime));
+}, 1000);
+
+function formatTime(date) {
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+    return hours + ":" + minutes;
+}
+
+function pad(number) {
+    if (number < 10) {
+        return "0" + number;
+    }
+    return number;
+}
