f2f2c1446219a7109e351dc17014deff33decff2
Bissiking <34923959+Bissiking@users.noreply.github.com>
2024-04-16 14:15:45 +0200

    5.1.1

--- javascripts/home/statut.js
+++ javascripts/home/statut.js
@@ -1,29 +1,52 @@
-// var url = "/data/1a0c7cba-cd1f-438e-8750-3d566cd81fdc/";
+function pingServer() {
+
+    let api_nino_txt = $('#api-nino-prod-txt');
+    let api_nino_cricle = $('#api-nino-prod-circle');
+
+
+    api_nino_cricle.removeClass();
+    api_nino_txt.text('Vérification du service');
+
+    $.ajax({
+        url: 'https://dev.nino.mhemery.fr/check',
+        type: 'GET',
+        timeout: 3000, // Définir un délai d'attente pour la réponse (en millisecondes)
+        success: function (response) {
+            console.log('Serveur accessible');
+            console.log(response);
+            // Faire quelque chose avec la réponse si nécessaire
+
+            api_nino_cricle.addClass('circle-online');
+            api_nino_txt.text('Service opérationnel');
+
+        },
+        error: function (xhr, status, error) {
+            console.error('Erreur lors du ping du serveur : ' + error);
+            // Gérer l'erreur ou afficher un message d'erreur
+
+            api_nino_cricle.addClass('circle-offline');
+            api_nino_txt.text('Service hors ligne');
+        }
+    });
+}
 
 function ServicesCheck() {
     let NbServiceSection = $('.serviceSection');
 
-    console.log(NbServiceSection);
-    console.log(NbServiceSection.length);
-
     for (let i = 0; i < NbServiceSection.length; i++) {
         let element = NbServiceSection[i];
         $(element).find('span.circle-stats').each(function () {
-            console.log($(this).attr('class'));
 
             const id = $(this).data('id');
             var service = $(this).data('service');
 
             let searchTerm = service.toLowerCase();
 
-            console.log(searchTerm);
-
             if (searchTerm.includes("plex")) {
                 service = "plex";
             } else if (searchTerm.includes("jelly")) {
                 service = "jellyfin";
             } else {
-                console.log('DEBUG');
                 let statsTxt = 'Service hors ligne';
                 let statsClass = "circle-offline";
                 let $currentElement = $(this);
@@ -57,8 +80,6 @@ function ServicesCheck() {
                         this.removeClass(statsRemove);
                         this.siblings('.txt-stats').text(statsTxt);
                     }
-
-                    console.log(response);
                 },
                 error: function (error) {
                     console.log(error);
@@ -74,7 +95,9 @@ function ServicesCheck() {
 
 // Fisrt Start
 ServicesCheck();
+pingServer();
 
 setTimeout(() => {
     ServicesCheck();
+    pingServer();
 }, 60000);
